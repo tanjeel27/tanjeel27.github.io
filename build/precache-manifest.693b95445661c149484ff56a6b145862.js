c97596eb2dc7304472cb29003bab3ee08cd1cee9
tanjeel27 <tanjeel27@gmail.com>
2020-07-04 01:35:36 +0600

    Changed Commit

---
+++ build/precache-manifest.693b95445661c149484ff56a6b145862.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab3b7edb6ef02667a12c0ace6707dbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a2efc9d6bc60319e6b2",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "d4ff827897ad6c7ae689",
+    "url": "/static/js/2.a0d64e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0d64e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2efc9d6bc60319e6b2",
+    "url": "/static/js/main.2cedb80f.chunk.js"
+  },
+  {
+    "revision": "7176620cb3c6ad9515b4",
+    "url": "/static/js/runtime-main.a5268b60.js"
+  },
+  {
+    "revision": "57e774187cf8a3f70e53b34248888d22",
+    "url": "/static/media/profile.57e77418.jpg"
+  }
+]);
